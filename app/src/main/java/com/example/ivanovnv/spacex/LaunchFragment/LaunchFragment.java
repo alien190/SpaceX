e16027821bbff6705434633d34281fb87bdfd439
alien1900 <alien1900@bk.ru>
2018-05-22 16:52:49 +0700

    try to use Flowable

--- app/src/main/java/com/example/ivanovnv/spacex/LaunchFragment/LaunchFragment.java
+++ app/src/main/java/com/example/ivanovnv/spacex/LaunchFragment/LaunchFragment.java
@@ -66,7 +66,6 @@ public class LaunchFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        updateAdapterFromDataBaseFlowable();
 
     }
 
