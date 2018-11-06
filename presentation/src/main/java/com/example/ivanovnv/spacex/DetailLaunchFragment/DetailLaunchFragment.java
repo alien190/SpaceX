b1b200330401ac95eef21664ab30baf9c3e328f1
alien190 <alien1900@bk.ru>
2018-11-06 15:41:26 +0700

    remove PDF viewer added CollapsedToolBar to LaunchDetailFragment

--- presentation/src/main/java/com/example/ivanovnv/spacex/DetailLaunchFragment/DetailLaunchFragment.java
+++ presentation/src/main/java/com/example/ivanovnv/spacex/DetailLaunchFragment/DetailLaunchFragment.java
@@ -4,24 +4,25 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
 import android.transition.TransitionInflater;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.data.database.LaunchDao;
 import com.example.data.utils.DbBitmapUtility;
 import com.example.domain.model.launch.DomainLaunch;
 import com.example.domain.service.ILaunchService;
 import com.example.ivanovnv.spacex.R;
-import com.github.barteksc.pdfviewer.PDFView;
-
-import java.io.InputStream;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import timber.log.Timber;
@@ -31,13 +32,30 @@ import toothpick.Toothpick;
 public class DetailLaunchFragment extends Fragment {
     private static final String FLIGHT_NUMBER_KEY = "com.example.ivanovnv.spacex.DetailLaunchFragment.KEY";
     //private TextView mFlightNumber;
-    private ImageView mImageView;
-    //    private TextView mMissionName;
-//    private TextView mRocketName;
-//    private TextView mLaunchDate;
-//    private TextView mDetails;
+    @BindView(R.id.iv_mission_icon)
+    public ImageView mImageView;
+    @BindView(R.id.tv_mission_name)
+    public TextView mMissionName;
+    //    private TextView mRocketName;
+    @BindView(R.id.tv_launch_date)
+    public TextView mLaunchDate;
+    @BindView(R.id.tv_details)
+    public TextView mDetails;
+    @BindView(R.id.collapsing_toolbar)
+    public CollapsingToolbarLayout mCollapsingToolbarLayout;
     private int mFlightNumberInt;
-    private PDFView mPDFViewer;
+
+    @BindView(R.id.tv_details2)
+    public TextView mDetails2;
+
+    @BindView(R.id.tv_details3)
+    public TextView mDetails3;
+
+    @BindView(R.id.tv_details4)
+    public TextView mDetails4;
+
+    @BindView(R.id.tv_details5)
+    public TextView mDetails5;
 
     @Inject
     ILaunchService mLaunchService;
@@ -69,14 +87,8 @@ public class DetailLaunchFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fr_detail_launch, container, false);
-//        mFlightNumber = view.findViewById(R.id.tv_flight_number);
-//        mMissionName = view.findViewById(R.id.tv_mission_name);
-        mImageView = view.findViewById(R.id.iv_mission_icon);
-        //mImageView.setTransitionName("t69");
-//        mRocketName = view.findViewById(R.id.tv_rocket_name);
-//        mLaunchDate = view.findViewById(R.id.tv_launch_date);
-//        mDetails = view.findViewById(R.id.tv_details);
-        mPDFViewer = view.findViewById(R.id.pdf_viewer);
+
+        ButterKnife.bind(this, view);
 
         Scope scope = Toothpick.openScope("Application");
         Toothpick.inject(this, scope);
@@ -85,7 +97,7 @@ public class DetailLaunchFragment extends Fragment {
         try {
             mFlightNumberInt = args.getInt(FLIGHT_NUMBER_KEY);
             mImageView.setTransitionName("TransitionName" + String.valueOf(mFlightNumberInt));
-            mDisposable = mLaunchService.getLaunchByFlightNumberWithPressKit(mFlightNumberInt)
+            mDisposable = mLaunchService.getLaunchByFlightNumber(mFlightNumberInt)
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(this::showLaunch, Timber::d);
         } catch (Throwable t) {
@@ -99,13 +111,14 @@ public class DetailLaunchFragment extends Fragment {
     private void showLaunch(DomainLaunch domainLaunch) {
         if (domainLaunch != null) {
             mImageView.setImageBitmap(DbBitmapUtility.getImage(domainLaunch.getImage()));
-            InputStream pressKitStream = domainLaunch.getPresskitStream();
-            if (pressKitStream != null) {
-                mPDFViewer.fromStream(pressKitStream)
-                        .onError(t -> Timber.d(t, "mPDFViewer.onError"))
-                        .onLoad(nbPages -> Timber.d("mPDFViewer.onComplete, pages:%d", nbPages))
-                        .load();
-            }
+            mMissionName.setText(domainLaunch.getMission_name());
+            mCollapsingToolbarLayout.setTitle(domainLaunch.getMission_name());
+            mDetails.setText(domainLaunch.getDetails());
+            mDetails2.setText(domainLaunch.getDetails());
+            mDetails3.setText(domainLaunch.getDetails());
+            mDetails4.setText(domainLaunch.getDetails());
+            mDetails5.setText(domainLaunch.getDetails());
+            mLaunchDate.setText(domainLaunch.getLaunch_date_utc());
         }
     }
 
